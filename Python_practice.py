ffb5a5c23264c82e17aaa1bafbfe2a90dd6bac05
Joseph Gallatin <frjg97@Joseph-Gallatins-iMac-August-2018.local>
2021-07-24 10:24:53 -0500

    initial commit of my assignment

---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+counties = ["Arapahoe", "Denver", "Jefferson"]
+for i in range(len(counties)):
+    print(counties[i])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict.keys():
+    print(county)
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+    
